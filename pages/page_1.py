1387d147378b96bc7ff92add8acc29be650400b1
mich360 <python3.supu_app.vtuber.n-masumoto@nifty.com>
2023-04-16 17:37:02 +0900

    first commit

---
+++ pages/page_1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+code = '''
+import streamlit as st
+
+st.title('サプーアプリ')
+'''
+st.code(code, language='python')
---
+++ pages/page_3.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd   
+import matplotlib.pyplot as plt  
+#データデータ分析関連
+df = pd.read_csv('./data/平均気温.csv', index_col='月')
+st.line_chart(df)
+st.bar_chart(df['2021年'])
+# st.dataframe(df)
+# st.table(df)
+# Matplotlibは、Pythonのデータ可視化ライブラリの1つ
+fig, ax = plt.subplots()
+ax.plot(df.index, df['2021年'])
+ax.set_title('matplotlib graph')
+st.pyplot(fig)
