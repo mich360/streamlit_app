daee652a5f4611a7e9c96676b8d665327de737b5
mich360 <65708357+mich360@users.noreply.github.com>
2023-04-17 12:47:07 +0900

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -4,7 +4,7 @@ import pandas as pd
 #データデータ分析関連
 df = pd.read_csv('./data/平均気温.csv', index_col='月')
 st.line_chart(df)
-st.bar_chart(df['2021年湘南の気温'])
+st.bar_chart(df['2021年'])
 # st.dataframe(df)
 # st.table(df)
 # Matplotlibは、Pythonのデータ可視化ライブラリの1つ
