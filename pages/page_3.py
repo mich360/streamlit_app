589d2b9cbc9b2bda67c79b9971a6cb7c2d863b0c
mich360 <65708357+mich360@users.noreply.github.com>
2023-04-17 12:03:52 +0900

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd   
-import matplotlib.pyplot as plt  
+#import matplotlib.pyplot as plt  
 #データデータ分析関連
 df = pd.read_csv('./data/平均気温.csv', index_col='月')
 st.line_chart(df)
@@ -8,7 +8,7 @@ st.bar_chart(df['2021年'])
 # st.dataframe(df)
 # st.table(df)
 # Matplotlibは、Pythonのデータ可視化ライブラリの1つ
-fig, ax = plt.subplots()
-ax.plot(df.index, df['2021年'])
-ax.set_title('matplotlib graph')
-st.pyplot(fig)
+# fig, ax = plt.subplots()
+# ax.plot(df.index, df['2021年'])
+# ax.set_title('matplotlib graph')
+# st.pyplot(fig)
