4735567faf2178aefe931cc9afe9550e071e358c
mich360 <65708357+mich360@users.noreply.github.com>
2023-04-17 12:37:22 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-st.title('サプーアプリ')
-st.caption('これはサプーの動画用のテストアプリです')
+st.title('サンプルアプリ')
+st.caption('これはテストアプリです')
            
 image = Image.open('./data/blue.png')
 st.image(image, width=400)
