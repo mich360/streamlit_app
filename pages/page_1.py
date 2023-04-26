81cc60004109e60c28b5bc828075d11046936517
Mich <65708357+mich360@users.noreply.github.com>
2023-04-26 12:27:22 +0900

    Update page_1.py
    code 追加

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,7 +1,15 @@
 import streamlit as st
 code = '''
 import streamlit as st
+from PIL import Image
 
-st.title('サプーアプリ')
+st.title('サンプルアプリ')
+st.header('ヘッダー')
+st.subheader('サブヘッダー')
+st.write('文字列　　') # markdown
+st.caption('これはテストアプリです')
+           
+image = Image.open('./data/blue.png')
+st.image(image, width=400)
 '''
 st.code(code, language='python')
