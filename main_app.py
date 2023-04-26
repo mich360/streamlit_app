ea9c573e11c89d48b9d1a03756033764311a9b2c
Mich <65708357+mich360@users.noreply.github.com>
2023-04-26 12:14:15 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,6 +3,10 @@ from PIL import Image
 
 st.title('サンプルアプリ')
 st.caption('これはテストアプリです')
+st.title('タイトル')
+st.header('ヘッダー')
+st.subheader('サブヘッダー')
+st.write('文字列') # markdown
            
 image = Image.open('./data/blue.png')
 st.image(image, width=400)
