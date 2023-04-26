2cda84c4ce7850845d07a1b28cd10c8f23b31593
Mich <65708357+mich360@users.noreply.github.com>
2023-04-26 12:17:58 +0900

    Update main_app.py
    文字関連表示

--- main_app.py
+++ main_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from PIL import Image
 
 st.title('サンプルアプリ')
-st.caption('これはテストアプリです')
-st.title('タイトル')
 st.header('ヘッダー')
 st.subheader('サブヘッダー')
 st.write('文字列') # markdown
+st.caption('これはテストアプリです')
            
 image = Image.open('./data/blue.png')
 st.image(image, width=400)
